0c1023a6b87d8a1ebcb933c4dceed23a8229624b
Zakaria Amarifi <z.amarifi@dm73.net>
2017-04-03 11:57:26 +0100

    explaining the builder pattern example

--- creational-patterns/03-builder-pattern/js/01-first-example.js
+++ creational-patterns/03-builder-pattern/js/01-first-example.js
@@ -1,5 +1,10 @@
 // In this example we will see an example of object created using a builder pattern
 
+// First we create an object (using the function method of course),
+// this object will create other objects in function of the type of builder we will assign to
+// the builder method 'construct'. This object will have only this method that handles
+// other builders having the same method names used in the main builder and
+// return the correct builder. This main object builder is usually called a Director
 function Shop() {
     this.construct = function(builder) {
         builder.step1();
@@ -8,6 +13,7 @@ function Shop() {
     }
 }
 
+// We create here the Car builder, and creating of course the methods used on the main builder
 function CarBuilder() {
     this.car = null;
 
@@ -24,6 +30,7 @@ function CarBuilder() {
     };
 }
 
+// We create here the Truck builder, and creating of course the methods used on the main builder
 function TruckBuilder() {
     this.truck = null;
 
@@ -40,6 +47,7 @@ function TruckBuilder() {
     };
 }
 
+// We create the Car Object used on the Car builder
 function Car() {
     this.doors = 0;
 
@@ -52,6 +60,7 @@ function Car() {
     };
 }
 
+// We create the Car Object used on the Truck builder
 function Truck() {
     this.doors = 0;
 
@@ -67,6 +76,7 @@ function Truck() {
 // log helper
 var log = (function() {
     var log = '';
+
     return {
         add: function(msg) {
             log += msg + '\n';
@@ -78,16 +88,24 @@ var log = (function() {
     }
 })();
 
-function run() {
-    var shop = new Shop();
-    var carBuilder = new CarBuilder();
-    var truckBuilder = new TruckBuilder();
-    var car = shop.construct(carBuilder);
-    var truck = shop.construct(truckBuilder);
+var shop = new Shop();
+var carBuilder = new CarBuilder();
+var truckBuilder = new TruckBuilder();
+var car = shop.construct(carBuilder);
+var truck = shop.construct(truckBuilder);
 
-    car.say();
-    truck.say();
+log.add('Is car instance of CarBuilder: ' + (car instanceof CarBuilder)); // false
+log.add('Is car instance of Car: ' + (car instanceof Car)); // true, it is what returned finally
+log.add('Is car instance of Shop: ' + (car instanceof Shop)); // false
 
-    log.show();
-}
-run();
+log.add('Is truck instance of TruckBuilder: ' + (truck instanceof TruckBuilder)); // false
+log.add('Is truck instance of Truck: ' + (truck instanceof Truck)); // true
+log.add('Is truck instance of Shop: ' + (truck instanceof Shop)); // false
+
+car.say();
+truck.say();
+
+log.show();
+
+// The builder pattern will use a main director object that uses sub-builders having methods
+// called on the main builder method, and return a final object that match
