a144b1be8f20ae65640cb85e09650dc7ed31da6e
Zakaria Amarifi <z.amarifi@dm73.net>
2017-04-14 09:50:20 +0100

    explaining the advantages of the singleton creational pattern

--- creational-patterns/05-singleton-pattern/js/01-basic-singleton.js
+++ creational-patterns/05-singleton-pattern/js/01-basic-singleton.js
@@ -1,12 +1,12 @@
-// In this example we will see an example of object created using a factory pattern
+// In this example we will see an example of object created using a singleton pattern
 
+// Let's create a singleton object that has a method that return the same instance
 var mySingleton = (function() {
-    // Instance stores a reference to the Singleton
+    // Instance stores a reference to the Singleton, and it is private
     var instance;
 
+    // Here, we create an instance of the Singleton object
     function init() {
-        // Singleton
-
         // Private methods and variables
         function privateMethod() {
             console.log('I am private');
@@ -17,7 +17,6 @@ var mySingleton = (function() {
         var privateRandomNumber = Math.random();
 
         return {
-
             // Public methods and variables
             publicMethod: function() {
                 console.log('The public can see me!');
@@ -36,33 +35,9 @@ var mySingleton = (function() {
         // or create one if it doesn't
         getInstance: function() {
             if (!instance) {
+                // The 'init' function is called only once
                 instance = init();
             }
-
-            return instance;
-        }
-    };
-})();
-
-var myBadSingleton = (function() {
-    // Instance stores a reference to the Singleton
-    var instance;
-
-    function init() {
-        // Singleton
-        var privateRandomNumber = Math.random();
-
-        return {
-            getRandomNumber: function() {
-                return privateRandomNumber;
-            }
-        };
-    };
-
-    return {
-        // Always create a new Singleton instance
-        getInstance: function() {
-            instance = init();
 
             return instance;
         }
@@ -74,11 +49,5 @@ var singleA = mySingleton.getInstance();
 var singleB = mySingleton.getInstance();
 console.log(singleA.getRandomNumber() === singleB.getRandomNumber()); // true
 
-var badSingleA = myBadSingleton.getInstance();
-var badSingleB = myBadSingleton.getInstance();
-console.log(badSingleA.getRandomNumber() === badSingleB.getRandomNumber()); // false
-
-// Note: as we are working with random numbers, there is a
-// mathematical possibility both numbers will be the same,
-// however unlikely. The above example should otherwise still
-// be valid.
+// This approach is very usefull to avoid creating useless instances. Also usefull to share
+// one instance for different objects. It's the goal of the 'Dependency Injection' approach
